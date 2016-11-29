fd49e16ac57319ff1b1da2b8c31db7b1b9691b96
Alexandro <arvitaly@list.ru>
2016-11-29 16:25:33 +0700

    fix author

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,7 +10,7 @@ module.exports = yeoman.Base.extend({
         this.destinationPath(f)
       )
     );
-    const packageContent = this.fs.readJSON(this.destinationPath('package.json')) || {};
+    const packageContent = this.fs.readJSON(this.destinationPath('package.json')) || {homepage: ''};
     packageContent.jest = {
       automock: true,
       unmockedModulePathPatterns: [
@@ -21,9 +21,13 @@ module.exports = yeoman.Base.extend({
     packageContent.scripts.test = 'tsc && tslint --project=tsconfig.json && jest --verbose';
     packageContent.scripts['watch:test'] = 'jest --watch';
     packageContent.devDependencies = packageContent.devDependencies || {};
+    const author = packageContent.homepage.indexOf('github.com') > -1 ? packageContent.homepage.match(/^https:\/\/github.com\/([^/]+)/gi)[0].substr(19) : '';
     this.fs.writeJSON(this.destinationPath('package.json'), packageContent);
     const readmeContent = this.fs.read(this.templatePath('README.md'));
-    this.fs.write(this.destinationPath('README.md'), readmeContent.replace(/&name&/gi, packageContent.name).replace(/&description&/gi, packageContent.description));
+    this.fs.write(this.destinationPath('README.md'), readmeContent
+      .replace(/&author&/gi, author)
+      .replace(/&name&/gi, packageContent.name)
+      .replace(/&description&/gi, packageContent.description));
     this.npmInstall(['coveralls', 'jest', 'typescript', 'tslint', '@types/jest'], {'save-dev': true});
   },
 
--- generators/app/templates/README.md
+++ generators/app/templates/README.md
@@ -21,3 +21,12 @@
 
     npm install
     npm test
+
+[npm-image]: https://badge.fury.io/js/&name&.svg
+[npm-url]: https://npmjs.org/package/&name&
+[travis-image]: https://travis-ci.org/&author&/&name&.svg?branch=master
+[travis-url]: https://travis-ci.org/&author&/&name&
+[daviddm-image]: https://david-dm.org/&author&/&name&.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/&author&/&name&
+[coveralls-image]: https://coveralls.io/repos/&author&/&name&/badge.svg
+[coveralls-url]: https://coveralls.io/r/&author&/&name&
